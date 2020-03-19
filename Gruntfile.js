9d78f77f9a39f5d2d45fb1408783aff4467b0630
zhangzhuozhang <2028251635@qq.com>
2020-03-19 21:12:28 +0800

    add

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+      htmlmin: {
+        options: {
+          collapseWhitespace: true,
+          preserveLineBreaks: false
+        },
+        files: {
+          src: './index.html',
+          dest: './dist/index.html'
+        }
+      },
+      cssmin: {  
+        './dist/main.css': 'main.css'
+      },
+      uglify: {
+        release:{
+          files: {
+            './dist/main.js': 'main.js'
+          }
+        }       
+      }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['htmlmin','cssmin','uglify:release']); 
+  };
